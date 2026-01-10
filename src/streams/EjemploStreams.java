34909180680c01c9a034b5c086a416ad5808e065
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-01-10 15:05:57 -0500

    Operador Map

--- src/streams/EjemploStreams.java
+++ src/streams/EjemploStreams.java
@@ -8,18 +8,23 @@ public class EjemploStreams {
     List<String> lista = Arrays.asList("Ana", "Luisa", "Merina", "Jhona", "Fiorella");
     ejemploForEach(lista);
     ejemploFilter(lista);
+    ejemploMap(lista);
+  }
 
+  private static void ejemploMap(List<String> lista) {
+    System.out.println("=Operador Map=");
+    lista.stream().map(nombre -> {
+      return nombre.toUpperCase();
+    }).forEach(System.out::println);
   }
 
   private static void ejemploFilter(List<String> lista) {
-    System.out.println("=Ejemplo de filter=");
-    lista.stream()
-            .filter(nombre -> nombre.length() >= 4)
-            .forEach(System.out::println);
+    System.out.println("=Operador filter=");
+    lista.stream().filter(nombre -> nombre.length() >= 4).forEach(System.out::println);
   }
 
   private static void ejemploForEach(List<String> lista) {
-    System.out.println("=Ejemplo de ForEach=");
+    System.out.println("=Operador ForEach=");
     lista.stream().forEach(s -> {
       System.out.println(s);
     });
